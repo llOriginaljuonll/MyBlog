a4836816b16d1aebe9ab1f511ccd0a34887b2aa0
llOriginaljuonll <lastsecond1989@gmail.com>
2023-11-14 04:39:25 +0700

    Crete static folder | Add installed bootstrap 5

--- Root_Directory/apps/Blog/views.py
+++ Root_Directory/apps/Blog/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def Blog(request):
-	return HttpResponse('<h1>Welcome My Blog</h1>')
+	return render(request, 'base.html')
 
