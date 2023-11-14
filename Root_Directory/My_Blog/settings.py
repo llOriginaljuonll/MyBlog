83e931706ccd483d33d22dabf80e75409f65a5ee
Originaljuon lastsecond1989@gmail.com <jisoooh0202@gmail.com>
2023-11-14 16:35:55 +0700

    Install crispy_bootstrap5 & Used in template

--- Root_Directory/My_Blog/settings.py
+++ Root_Directory/My_Blog/settings.py
@@ -42,8 +42,10 @@ INSTALLED_APPS = [
 	"apps.Blog",
 
 	# Third-party apps
-	"crispy_forms"
+	"crispy_forms",
+	"crispy_bootstrap5",
 ]
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -131,3 +133,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
