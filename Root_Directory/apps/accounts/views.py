bf6eb0b9fa9718f2b8d729b97b5913bd6269e013
llOriginaljuonll <lastsecond1989@gmail.com>
2023-11-17 16:33:44 +0700

    Create sign in system & Style
    มีหน้าล็อกอิน กับหน้าลิสต์บทความที่ style

--- Root_Directory/apps/accounts/views.py
+++ Root_Directory/apps/accounts/views.py
@@ -1,1 +1,21 @@
 from django.shortcuts import render, redirect
+
+# accounts system
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
+
+def sign_in(request):
+    if request.method == 'POST':
+
+        # AuthenticationForm is buit-in funcion.
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'accounts/sign_in.html', {
+        'form': form
+    })
