847b82c4f1251542f5218e9dab88a2ed6f803d73
llOriginaljuonll <lastsecond1989@gmail.com>
2023-11-18 16:19:22 +0700

    Create sign up system

--- Root_Directory/apps/accounts/urls.py
+++ Root_Directory/apps/accounts/urls.py
@@ -3,5 +3,6 @@ from . import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', views.sign_in, name='sign_in')
+    path('', views.sign_in, name='sign_in'),
+    path('sign_up/', views.sign_up, name='sign_up'),
 ]
--- Root_Directory/apps/accounts/views.py
+++ Root_Directory/apps/accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 # accounts system
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login, logout
 
 def sign_in(request):
@@ -18,4 +18,15 @@ def sign_in(request):
     
     return render(request, 'accounts/sign_in.html', {
         'form': form
+def sign_up(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/sign_up.html', {
+        'form': form,
     })
