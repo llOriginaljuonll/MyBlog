09bf351eeda3b93b511f833c051f4cbd945b74c8
Originaljuon lastsecond1989@gmail.com <jisoooh0202@gmail.com>
2023-11-21 13:25:22 +0700

    Create blog-delete

--- Root_Directory/apps/Blog/urls.py
+++ Root_Directory/apps/Blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
 	path('article/', views.article_form, name='article_form'),
     path('detail/<int:blog_id>', views.blog_detail, name='blog_detail'),
     path('edit/<int:blog_id>', views.blog_edit, name='blog_edit'),
+    path('delete/<int:blog_id>', views.blog_delete, name='blog_delete'),
 ]
--- Root_Directory/apps/Blog/views.py
+++ Root_Directory/apps/Blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import WriterBlogForm
 from .models import WriterBlog
+from django.contrib import messages
 
 def blog_home(request):
 	return render(request, 'base.html')
@@ -26,10 +27,16 @@ def blog_detail(request, blog_id):
 	return render(request, 'blog_detail.html', {'blog': blog})
 
 def blog_edit(request, blog_id):
-	blog_detail = WriterBlog.objects.get(pk=blog_id)
-	form = WriterBlogForm(request.POST or None, instance=blog_detail)
+	blog = WriterBlog.objects.get(pk=blog_id)
+	form = WriterBlogForm(request.POST or None, instance=blog)
 
 	if form.is_valid():
 		form.save()
 		return redirect('/')
 	return render(request, 'blog_edit.html', {'form': form})
+
+def blog_delete(request, blog_id):
+	blog = WriterBlog.objects.get(pk=blog_id)
+	blog.delete()
+	messages.success(request, 'Blog has been successfully deleted.')
+	return redirect('/')
