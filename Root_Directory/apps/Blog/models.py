2d2319876f93a686c93ba0191de6389f0f15441a
llOriginaljuonll <lastsecond1989@gmail.com>
2023-11-23 16:13:58 +0700

    Create customuser model

--- Root_Directory/apps/Blog/models.py
+++ Root_Directory/apps/Blog/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
 class Blog(models.Model):
 	id: int
 	article_name = models.CharField(max_length=255)
 	content = models.TextField()
 	created = models.DateTimeField(auto_now_add=True)
-	writer = models.ForeignKey(User, on_delete=models.CASCADE,)
+	writer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
 	views = models.IntegerField(default=0)
+	likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name= 'blogpost_like')
+
 
 	class Meta:
 		verbose_name = "ข้อมูลบทความ"
@@ -14,3 +17,6 @@ class Blog(models.Model):
 
 	def __str__(self):
 		return self.article_name
+	
+	def number_of_likes(self):
+		return self.likes.count()
