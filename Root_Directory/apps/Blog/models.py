3c01adc3891d5081d8b118981fea7cb93cd1160b
llOriginaljuonll <lastsecond1989@gmail.com>
2023-11-24 11:16:18 +0700

    Add 'liked' field and Creat 'Like' model.

--- Root_Directory/apps/Blog/models.py
+++ Root_Directory/apps/Blog/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
+
 class Blog(models.Model):
 	id: int
 	article_name = models.CharField(max_length=255)
 	content = models.TextField()
 	created = models.DateTimeField(auto_now_add=True)
-	writer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
+	writer = models.ForeignKey(User , on_delete=models.CASCADE,)
 	views = models.IntegerField(default=0)
-	likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name= 'blogpost_like')
+	liked = models.ManyToManyField(User , default=None, blank=True, related_name= 'liked')
 
 
 	class Meta:
@@ -18,5 +19,23 @@ class Blog(models.Model):
 	def __str__(self):
 		return self.article_name
 	
+	@property
+	def num_likes(self):
+		return self.liked.all().count()
+	
 	def number_of_likes(self):
 		return self.likes.count()
+	
+	    
+LIKE_CHOICES = (
+    ('Like', 'Like'),
+    ('Unlike', 'Unlike'),
+)
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    value = models.CharField(choices=LIKE_CHOICES, default='Like', max_length=10)
+
+    def __str__(self):
+        return str(self.post)
