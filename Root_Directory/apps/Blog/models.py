0315ee10c92a427880d7146023b14da890fd9d45
llOriginaljuonll <lastsecond1989@gmail.com>
2023-11-28 16:29:25 +0700

    Edit Blog model

--- Root_Directory/apps/Blog/models.py
+++ Root_Directory/apps/Blog/models.py
@@ -6,10 +6,9 @@ class Blog(models.Model):
 	article_name = models.CharField(max_length=255)
 	content = models.TextField()
 	created = models.DateTimeField(auto_now_add=True)
-	writer = models.ForeignKey(User , on_delete=models.CASCADE,)
+	writer = models.ForeignKey(User, on_delete=models.CASCADE,)
 	views = models.IntegerField(default=0)
-	liked = models.ManyToManyField(User , default=None, blank=True, related_name= 'liked')
-
+	likes = models.ManyToManyField(User, related_name='blog_posts')
 
 	class Meta:
 		verbose_name = "ข้อมูลบทความ"
@@ -18,23 +17,6 @@ class Blog(models.Model):
 	def __str__(self):
 		return self.article_name
 	
-	@property
-	def num_likes(self):
-		return self.liked.all().count()
-	
-	def number_of_likes(self):
+	def total_likes(self):
 		return self.likes.count()
 	
-	    
-LIKE_CHOICES = (
-    ('Like', 'Like'),
-    ('Unlike', 'Unlike'),
-)
-
-class Like(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Blog, on_delete=models.CASCADE)
-    value = models.CharField(choices=LIKE_CHOICES, default='Like', max_length=10)
-
-    def __str__(self):
-        return str(self.post)
