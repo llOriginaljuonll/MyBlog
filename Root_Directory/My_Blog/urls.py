1f726bf9a07af4ee12894d109f1d7b1a0c63c34b
llOriginaljuonll <lastsecond1989@gmail.com>
2023-12-09 11:41:58 +0700

    Add media_folder and style user_page.

--- Root_Directory/My_Blog/urls.py
+++ Root_Directory/My_Blog/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 	path("", include(("apps.blog.urls", "blog"), namespace="blog")),
 	path("accounts/", include(("apps.accounts.urls", "accounts"), namespace="accounts")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
