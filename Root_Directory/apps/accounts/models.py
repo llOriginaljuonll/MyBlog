8624d174128cabbf8404255d3981d4a49e1bcf54
llOriginaljuonll <lastsecond1989@gmail.com>
2023-12-09 11:40:33 +0700

    Edit birthday_field and  create profile_img_field

--- Root_Directory/apps/accounts/models.py
+++ Root_Directory/apps/accounts/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     id: int
-    # birthday = models.DateField(null=True)
-    profile_img = models.ImageField(null=True, blank=True)
+    birthday = models.DateField(null=True, blank=True)
+    profile_img = models.ImageField(upload_to='upload/', null=True, blank=True)
 
     def __str__(self):
         return self.username
