98e8760c760808d92cb52cd2820819dd27f0c1e9
Originaljuon lastsecond1989@gmail.com <jisoooh0202@gmail.com>
2023-12-11 16:30:57 +0700

    Custom signup_user_form and create func. about bookmark

--- Root_Directory/apps/accounts/views.py
+++ Root_Directory/apps/accounts/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http.response import HttpResponseRedirect
 
 # User model
 from .models import CustomUser
+from .forms import CustomUserCreationForm
+from apps.blog.models import Blog
 
 # accounts system
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
 
 def sign_in(request):
@@ -29,13 +32,13 @@ def sign_out(request):
 
 def sign_up(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('/')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'accounts/sign_up.html', {
         'form': form,
     })
@@ -45,6 +48,18 @@ def user_info(request, user_id):
     context = {'client': user_info}
     return render(request, 'accounts/user_info.html', context)
 
-def user_age(request):
-    current_time = User.username
-    return render(request, 'accounts/user_info.html', {'now': current_time})
+# def user_age(request):
+#     current_time = User.username
+#     return render(request, 'accounts/user_info.html', {'now': current_time})
+
+def favourite_add(request, id):
+    post = get_object_or_404(Blog, id=id)
+    if Blog.bookmark_article.filter(id=request.user.id).exists():
+        Blog.bookmark_article.remove(request.user)
+    else:
+        Blog.bookmark_article.add(request.user)
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+def bookmark_list(request):
+    new = Blog.objects.get(bookmark_article=request.user)
+    return render(request, 'accounts/bookmark.html', {'new': new})
