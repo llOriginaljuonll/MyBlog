c65849d7556722a21d0719f787ee1636d72fea95
llOriginaljuonll <lastsecond1989@gmail.com>
2024-01-23 13:08:10 +0700

    create model Comment

--- Root_Directory/apps/Blog/models.py
+++ Root_Directory/apps/Blog/models.py
@@ -29,3 +29,12 @@ class Blog(models.Model):
 	def total_likes(self):
 		return self.likes.count()
 	
+class Comment(models.Model):
+	blog = models.ForeignKey(Blog, related_name="comments", on_delete=models.CASCADE)
+	name = models.CharField(max_length=255)
+	body = models.TextField()
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return '%s - %s' % (self.blog.article_name, self.name)
+	
